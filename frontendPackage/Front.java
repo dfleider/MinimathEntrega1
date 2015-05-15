b13d21d9e156b3c03348ba3615a0865233c57b08
dfleider <dfleiderman@gmail.com>
2015-05-15 18:40:27 -0300

    hola

--- frontendPackage/Front.java
+++ frontendPackage/Front.java
@@ -16,7 +16,6 @@ import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
