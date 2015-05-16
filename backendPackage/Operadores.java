9f091c42230c6522764d80729273e1be6cb23877
DiegoSteinsapir <dasteinsapir@uc.cl>
2015-05-16 17:47:10 -0300

    Clase Operadores

---
+++ backendPackage/Operadores.java
@@ -0,0 +1,39 @@
+package backendPackage;
+
+public class Operadores {
+
+	public Operadores()
+	{	}
+	
+	public boolean esNumero(String numero) // De: http://stackoverflow.com/questions/11241690/regex-for-checking-if-a-string-is-strictly-alphanumeric
+	{												// y: http://stackoverflow.com/questions/21704583/regex-for-documentfilter-to-match-all-decimal-number-but-also-number-with-just-a
+	    String pattern= "^[0-9]+[.]?[0-9]{0,}$"; // "^[0-9]*$";
+        if(numero.matches(pattern)){
+            return true;
+        }
+        return false;  
+	}
+	
+	public boolean esVariable(String numero) // De: http://stackoverflow.com/questions/11241690/regex-for-checking-if-a-string-is-strictly-alphanumeric
+	{												// y: http://stackoverflow.com/questions/21704583/regex-for-documentfilter-to-match-all-decimal-number-but-also-number-with-just-a
+	    String pattern= "^[a-zA-Z0-9]*$";//"^[0-9]+[.]?[0-9]{0,}$"; // "^[0-9]*$";
+        if(numero.matches(pattern) && !this.esNumero(numero)){
+            return true;
+        }
+        return false;  
+	}
+	
+	public boolean esOperador(String numero)
+	{
+		if (numero.equals("+") || numero.equals("-") ||
+				numero.equals("*") || numero.equals("/")
+				|| numero.equals("^"))
+		{
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+	
+}
