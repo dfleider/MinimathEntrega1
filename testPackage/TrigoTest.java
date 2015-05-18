410dedc65265d6d98873d9a6c7830f474e9f40d3
favazquez <favazquez@uc.cl>
2015-05-18 19:34:04 -0300

    Limpieza

--- testPackage/TrigoTest.java
+++ testPackage/TrigoTest.java
@@ -16,14 +16,6 @@ public class TrigoTest {
 				assertEquals("1.0",resultado);				
 	}
 	
-	@Test
-	public void testDouble(){
-				BackendMain b = new BackendMain();
-				b.main("fun f(x)=x-5");
-				b.main("fun g(x)=x+5");
-				String resultado=b.main("eval f(6)+g(6)");
-				assertEquals("12.0",resultado);				
-	}
 	
 		
 	
